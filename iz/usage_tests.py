ba4a550bb528ee875ae0c6ddd26cd8dcaf78d4ca
alexhallam <alexhallam6.28@gmail.com>
2023-08-31 12:22:20 -0400

    additional files and usage test

--- iz/usage_tests.py
+++ iz/usage_tests.py
@@ -15,7 +15,28 @@
 
 import pandas as pd
 import iz as iz
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("iz/data/fishSize.csv")
-fish_metalog = iz.metalog(y=df.FishSize, bounds=[0], boundedness='sl', terms=3, step_len=.01, epochs=1000, lr = 0.1)
+fish_metalog = iz.metalog(y=df.FishSize, bounds=[0,60], boundedness='b', terms=3, step_len=.001, epochs=500, lr = 0.1)
 iz.summary(fish_metalog)
+print(df.sort_values(by='FishSize').reset_index(drop=True))
+
+plt.hist(df.FishSize, 12)
+
+r_gens = iz.rmetalog(fish_metalog, n=5000, generator="hdr")
+# plt.hist(r_gens, 12)
+# plt.show()
+
+
+# quantiles from a percentile
+qs = iz.qmetalog(fish_metalog, y=[0.25, 0.5, 0.999])
+print("qmetalog demo: " + str(qs))
+
+# probabilities from a quantile
+ps = iz.pmetalog(fish_metalog, q=[3, 10, 35])
+print("pmetalog demo: " + str(ps))
+
+# density from a quantile
+ds = iz.dmetalog(fish_metalog, q=[3, 10, 25])
+print("dmetalog demo: " + str(ds))
